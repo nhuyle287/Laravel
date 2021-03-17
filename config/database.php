3c3a620ab53650936616f82304728305b8570ff0
nhuyle287 <50352152+nhuyle287@users.noreply.github.com>
2021-03-18 00:48:36 +0700

    config

--- config/database.php
+++ config/database.php
@@ -80,7 +80,7 @@ return [
 //            'username' => env('DB_USERNAME', 'forge'),
 //
 //            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
+            'charset' => 'utf8mb4',
 //            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
